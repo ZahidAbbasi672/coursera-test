d23625669fbe5361bfca97a109bdd78b5df58d89
zahid <zahid@home.com>
2024-03-24 23:31:52 +0500

    Solution Asignment1

---
+++ Asignments/Module_Solution1/app.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    
+
+
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+  
+  LunchCheckController.$inject = ['$scope'];
+  
+  function LunchCheckController($scope) {
+    $scope.lunchItems = "";
+    $scope.message = "";
+    
+    $scope.checkLunch = function() {
+      var items = $scope.lunchItems.split(',');
+      var itemCount = items.length;
+      
+      if ($scope.lunchItems.trim() === "") {
+        $scope.message = "Please enter data first";
+      } else if (itemCount <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+    };
+  }
+
+  
+    
+    })();
+    
+    
